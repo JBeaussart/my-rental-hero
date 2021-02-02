3b0ced1600d320056619de90f7676a329aad0b6b
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-02-02 21:02:47 +0100

    creating seed

--- app/controllers/super_heros_controller.rb
+++ app/controllers/super_heros_controller.rb
@@ -4,6 +4,44 @@ class SuperHerosController < ApplicationController
   end
 
   def show
+    @super_hero = SuperHero.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @super_hero = SuperHero.new
+  end
+
+  def create
+    SuperHero.new(super_hero_params)
+    @user = User.find(params[:user_id])
+    @super_hero.user = @user
+    if @super_hero.save
+      redirect_to super_heros_path
+    else
+      render :new
+    end
+  end
+
+  def edit
     @super_heros = SuperHero.find(params[:id])
+  end
+
+  def update
+    @super_heros = SuperHero.find(params[:id])
+    @super_heros.update(super_hero_params)
+    redirect_to super_heros_path(super_heros)
+  end
+
+  def destroy
+    @super_heros = SuperHero.find(params[:id])
+    @super_heros.destroy
+    redirect_to super_heros_path(@super_hero.user)
+  end
+
+  private
+
+  def super_hero_params
+    params.require(:super_heros).permit(:nickname, :description)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 puts "Cleaning database..."
 SuperHero.destroy_all
+# User.destroy_all
 
+puts "Creating User..."
+jerem = User.create!( first_name: 'jeremy', email: 'totodjbfjbf@gmail.fr', password: '123456')
 puts "Creating SuperHeros..."
-ironman = SuperHero.create([{ nickname: 'Iron man' }, { description: 'Je suis un genie milliardaire' }])
+ironman = SuperHero.create!( nickname: 'Iron man' ,  description: 'Je suis un genie milliardaire', user_id: jerem.id )
 
 puts "Finished!"
