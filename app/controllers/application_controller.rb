212af89c7009ee608199cf54c5bb4b0906ea10ad
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-02 20:27:05 +0100

    Add before action in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :company, :password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company])
   end
 end
