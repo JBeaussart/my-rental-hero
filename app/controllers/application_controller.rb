10161072c62906c21f7b65645fc5002424e08c42
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-02 20:18:01 +0100

    Add users resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :company, :password) }
+  end
 end
