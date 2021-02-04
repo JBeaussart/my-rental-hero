d640ab63287c16aed0abf936730517396b33c2f2
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-02-04 20:50:52 +0100

    checkpoint 21 bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,40 @@
 class BookingsController < ApplicationController
   def create
-    @super_hero = Super_hero.find(:super_hero_id)
+    @super_hero = Super_hero.find(params[:user][:booking][:super_hero_id])
     @booking = Booking.new
     @booking.superhero = @super_hero
+    if @booking.save
+      redirect_to bookings_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @booking = Booking.new
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+  def booking_params
+    params.require(:booking)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit, :update, :destroy] do
     resources :super_heros
-    resources :bookings, only: [:create]
+    resources :bookings, except: [:destroy]
   end
+  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
