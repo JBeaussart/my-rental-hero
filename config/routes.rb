45cee0ec08a40e731735ca83f2f2f0abbb42374c
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-02-04 21:00:47 +0100

    all is done!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :super_heros, except: [:destroy, :edit, :update]
+    resources :super_heros, except: [:destroy, :edit, :update, :show]
   end
- resources :super_heros, only: [:destroy, :edit, :update]
+ resources :super_heros, only: [:destroy, :edit, :update, :show]
 end
