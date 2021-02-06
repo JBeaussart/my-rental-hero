d3ed336325d285d20aa27a2bb60f8c4a312302f5
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-02-06 15:21:21 +0100

    db migrate

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
-end
+    def create
+      @super_hero = SuperHero.find(params[:super_hero_id])
+      @booking = Booking.new(booking_params)
+      @booking.super_hero_id = @super_hero.id
+      @booking.user = current_user
+      if @booking.save
+        redirect_to super_heros_path(@super_heros)
+      else
+        render :new
+      end
+    end
+
+  #   def show
+  #     @booking = Booking.find(params[:id])
+  #   end
+
+  #   def update
+  #     @booking = Booking.find(params[:id])
+  #     @booking.update(booking_params)
+  #     redirect_to user_super_hero_path(@super_hero)
+  #   end
+
+    def destroy
+      @booking = Booking.find(params[:id])
+      @booking.destroy
+      redirect_to user_super_hero_path(@super_heros)
+    end
+
+    private
+
+    def booking_params
+      params.require(:booking).permit(:starting_date, :ending_date)
+    end
+
+  end
