d56d29a74175c996a26dd5d9d0170c7e4c7b885d
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-02-12 15:19:51 +0100

    validates available on the way

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :super_hero
   belongs_to :user
+
+  validates :starting_date, :ending_date, presence: true, availability: true
+  validates :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if ending_date.blank? || starting_date.blank?
+
+    if ending_date < starting_date
+      errors.add(:ending_date, "must be after the start date")
+    end
+  end
 end
--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,4 +1,10 @@
 class SuperHero < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  def unavailable_dates
+    bookings.pluck(:starting_date, :ending_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
