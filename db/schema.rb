492cb463c98756b418edbbb25c5897dd1d2bae82
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-15 20:42:40 +0100

    Add field address to super_heros table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_142025) do
+ActiveRecord::Schema.define(version: 2021_02_15_194101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_142025) do
     t.integer "price_cents"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
     t.index ["user_id"], name: "index_super_heros_on_user_id"
   end
 
