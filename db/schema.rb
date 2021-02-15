97b8379f8dd829673277d5fc7b3c23ba9d9f509b
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-15 20:46:42 +0100

    Add latitude/longitude fields to super_heros table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_194101) do
+ActiveRecord::Schema.define(version: 2021_02_15_194513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_02_15_194101) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_super_heros_on_user_id"
   end
 
