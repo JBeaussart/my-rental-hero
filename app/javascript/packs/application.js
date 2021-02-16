8df6e8580f0228629c9c077234b33b12f4564da9
Thomas Pozzobon <thomaspozzobon86@gmail.com>
2021-02-16 20:01:48 +0100

    Delete alert timeout in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,10 +37,3 @@ document.addEventListener('turbolinks:load', () => {
   initAutocomplete();
   initMapbox();
 });
-
-// Timeout to alert pop up
-// window.setTimeout(function() {
-//     $(".alert").fadeTo(500, 0).slideUp(500, function(){
-//         $(this).remove();
-//     });
-// }, 4000);
