ebeb949da8de9d8db311fc4dc2de7b2be44f9645
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-02-19 18:29:32 +0100

    add dependent: :destroy on has_many bookings in super hero model

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,7 +1,7 @@
 class SuperHero < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address? 
